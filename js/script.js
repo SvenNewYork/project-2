0421755baa9505863d76e67e619326698a428ac6
Sven Thoenelt <sventhoenelt@gmx.de>
2018-11-20 22:28:23 +0000

    Version-2019-11-20

--- js/script.js
+++ js/script.js
@@ -50,5 +50,74 @@ $(document).ready(function(){
         }
         event.stopPropagation();
     });
-});
 
+// Travelling Drag and Drop Function
+
+  $(drag);
+    function drag(){
+      $(".picture").draggable({
+        revert: 'invalid',
+        stop: function(){
+          $(this).draggable('option','revert','invalid');
+        }
+        });
+
+      $(".rahmen").droppable({
+         drop: handleDrop
+      });
+
+      function handleDrop(event, ui){
+        var vacationSpot = ui.draggable.data().lead;
+        if (vacationSpot == "1" ){
+          ui.draggable.draggable('option','revert',true);
+          $(".travelStory").removeClass("travelStoryHide");
+          $(".travelStory").removeClass("travelStoryShow");
+          $(".travelStory").addClass("travelStoryHide");
+          $("#newZealand").removeClass("travelStoryHide");
+          $("#newZealand").addClass("travelStoryShow");
+          $("#worldMap").attr("src","images/Weltkarte-blank-newZealand.jpg");
+        }else if (vacationSpot == "2" ){
+          ui.draggable.draggable('option','revert',true);
+          $(".travelStory").removeClass("travelStoryHide");
+          $(".travelStory").removeClass("travelStoryShow");
+          $(".travelStory").addClass("travelStoryHide");
+          $("#morocco").removeClass("travelStoryHide");
+          $("#morocco").addClass("travelStoryShow");
+          $("#worldMap").attr("src","images/Weltkarte-blank-morocco.jpg");
+        }else if (vacationSpot == "3" ){
+          ui.draggable.draggable('option','revert',true);
+          $(".travelStory").removeClass("travelStoryHide");
+          $(".travelStory").removeClass("travelStoryShow");
+          $(".travelStory").addClass("travelStoryHide");
+          $("#peruAndBolivia").removeClass("travelStoryHide");
+          $("#peruAndBolivia").addClass("travelStoryShow");
+          $("#worldMap").attr("src","images/Weltkarte-blank-peruAndBolivia.jpg");
+        }else if (vacationSpot == "4" ){
+          ui.draggable.draggable('option','revert',true);
+          $(".travelStory").removeClass("travelStoryHide");
+          $(".travelStory").removeClass("travelStoryShow");
+          $(".travelStory").addClass("travelStoryHide");
+          $("#france").removeClass("travelStoryHide");
+          $("#france").addClass("travelStoryShow");
+          $("#worldMap").attr("src","images/Weltkarte-blank-france.jpg");
+        }else if (vacationSpot == "5" ){
+          ui.draggable.draggable('option','revert',true);
+          $(".travelStory").removeClass("travelStoryHide");
+          $(".travelStory").removeClass("travelStoryShow");
+          $(".travelStory").addClass("travelStoryHide");
+          $("#usaWestcoast").removeClass("travelStoryHide");
+          $("#usaWestcoast").addClass("travelStoryShow");
+          $("#worldMap").attr("src","images/Weltkarte-blank-usa.jpg");
+        }else if (vacationSpot == "6" ){
+          ui.draggable.draggable('option','revert',true);
+          $(".travelStory").removeClass("travelStoryHide");
+          $(".travelStory").removeClass("travelStoryShow");
+          $(".travelStory").addClass("travelStoryHide");
+          $("#usaAlaska").removeClass("travelStoryHide");
+          $("#usaAlaska").addClass("travelStoryShow");
+          $("#worldMap").attr("src","images/Weltkarte-blank-usaAlaska.jpg");
+        }
+      };
+    };
+    // END Travelling Drag and Drop Function
+});
